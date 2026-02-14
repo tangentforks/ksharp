662e586fac04cfd5ec98fe6ef0d90e01a323d96c
Eusebio Rufian-Zilbermann <erufian.github@rufian.zilbermann.com>
2026-02-13 19:21:09 -0500

    remove unneeded test file

--- debug_test.cs
+++
@@ -1,8 +0,0 @@
-// Debug test for character vector length
-using K3CSharp.Serialization;
-using System;
-
-var serializer = new KSerializer();
-var result = serializer.SerializeCharacterVector("");
-Console.WriteLine($"Length: {result.Length}");
-Console.WriteLine($"Bytes: {BitConverter.ToString(result).Replace("-", "")}");
